74e16a38f2bd4ac63c4aab34cf761722434d7eb2
aki3100 <velvetvoice2@gmail.com>
2023-02-13 20:38:01 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
   resources :rooms, only: [:new, :create]
+  end
 end
